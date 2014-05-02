4b2a1cb8be843a0339832202092558ce7b60dbfb
Theodore Calmes <theo@thoughtbot.com>
2014-05-02 14:20:54 -0700

    Initial Commit

---
+++ iScream/TCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TCAppDelegate.h
+//  iScream
+//
+//  Created by Theodore Calmes on 5/2/14.
+//  Copyright (c) 2014 theo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iScreamTests/iScreamTests.m
@@ -0,0 +1,34 @@
+//
+//  iScreamTests.m
+//  iScreamTests
+//
+//  Created by Theodore Calmes on 5/2/14.
+//  Copyright (c) 2014 theo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iScreamTests : XCTestCase
+
+@end
+
+@implementation iScreamTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
